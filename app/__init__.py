9e2de2c3605641ae4f94de287871f8059a65fe25
jaredap1995 <jaredaperez1995@gmail.com>
2023-11-26 18:36:52 -0800

    Initial Commit V2

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import sys
-print(sys.path)
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jaredp:secret@localhost/strongapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+CORS(app)
 
 from app import routes
