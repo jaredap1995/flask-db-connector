562c2572378e136a25e16423b82b9c08db920276
jaredap1995 <jaredaperez1995@gmail.com>
2023-11-27 21:12:43 -0800

    slight modifications to create new user and successfully able to use API to POST and GET from database

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app.models import Users
 @app.route("/user", methods=['POST'])
 def add_user():
     data = request.json
-    new_user = Users(username = data['username'], email = data['email'])
+    new_user = Users(username = data['username'], email = data['email'], password = data['password'])
     db.session.add(new_user)
     db.session.commit()
     return jsonify({"message": "User Added"}), 201
@@ -17,4 +17,16 @@ def get_user(username):
     if user:
         return jsonify({"username": user.username, "email": user.email})
     return jsonify({"message": "User not found"}), 404
+
+@app.route("/user/<username>", methods = ['PUT'])
+def update_user(username):
+    data = request.json()
+    user = Users.query.filter_by(username=username).first()
+    if user:
+        user.email = data.get('email', user.email)
+        db.session.commit()
+        return jsonify({ "message": "User updated"}), 200
+    else:
+        return jsonify({ "message": "User not found"}), 404
+
 
