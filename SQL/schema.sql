bf9a1e064493a93e5fc347fe41e9186064196080
jaredap1995 <jaredaperez1995@gmail.com>
2023-12-07 12:52:17 -0800

    need to update schema to include user_id for put request

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE strongapp;
 
 DROP TABLE IF EXISTS users; 
-CREATE TABLE users(
+CREATE TABLE raw_data(
     id serial primary key,
     first_name varchar(256) NOT NULL,
     last_name varchar(256) NOT NULL,
--- app/routes.py
+++ app/routes.py
@@ -18,18 +18,31 @@ def add_user():
     db.session.commit()
     return jsonify({"message": "User Added"}), 201
 
-@app.route("/user/<username>", methods = ['GET'])
-def get_user(username):
-    user = Users.query.filter_by(username = username).first()
+@app.route("/user/<value>", methods = ['GET'])
+def get_user(value):
+    user = Users.query.filter_by(username = value).first()
+    if not user:
+        user = Users.query.filter_by(email = value).first()
     if user:
-        return jsonify({"username": user.username, "email": user.email})
+        return jsonify({
+            "username": user.username, 
+            "email": user.email,
+            "firstname": user.first_name,
+            "lastname": user.last_name,
+            "phone": user.phone,
+            "password": user.password #To be removed
+            })
     return jsonify({"message": "User not found"}), 404
 
-@app.route("/user/<username>", methods = ['PUT'])
-def update_user(username):
+@app.route("/user/<int:user_id>", methods = ['PUT'])
+def update_user(user_id):
     data = request.json()
-    user = Users.query.filter_by(username=username).first()
+    user = Users.query.get(user_id)
     if user:
+        user.username = data.get('username', user.username)
+        user.first_name = data.get('firstname', user.firstname)
+        user.last_name = data.get('firstname', user.lastname)
+        user.phone = data.get('phone', user.phone)
         user.email = data.get('email', user.email)
         db.session.commit()
         return jsonify({ "message": "User updated"}), 200
